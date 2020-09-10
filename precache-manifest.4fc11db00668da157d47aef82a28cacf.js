d8b83548f46f52a537c004d43cddb9250d46d28c
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-09-10 12:23:30 +0200

    Updates

---
+++ precache-manifest.4fc11db00668da157d47aef82a28cacf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4567688f18324ea1ed51ed1992d0630d",
+    "url": "/life-game/index.html"
+  },
+  {
+    "revision": "3b463030e621f6afe502",
+    "url": "/life-game/static/css/main.cdefd3b9.chunk.css"
+  },
+  {
+    "revision": "066f99cef29f0885724c",
+    "url": "/life-game/static/js/2.5759e6e6.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/life-game/static/js/2.5759e6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b463030e621f6afe502",
+    "url": "/life-game/static/js/main.32689f19.chunk.js"
+  },
+  {
+    "revision": "d47b1c2acdf9c9edc7146f1e13f2addb",
+    "url": "/life-game/static/js/main.32689f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ea4098e1c822070166",
+    "url": "/life-game/static/js/runtime-main.1aa29e34.js"
+  },
+  {
+    "revision": "18b22fd673a0efbdc194f5776ac4ea30",
+    "url": "/life-game/static/media/gameCenter.18b22fd6.svg"
+  },
+  {
+    "revision": "a7bfe48cb4c8a2aaceb3186d3eea768a",
+    "url": "/life-game/static/media/heart-triangles.a7bfe48c.svg"
+  },
+  {
+    "revision": "9d37f4777c149fc815bc5e8bd9e5cb23",
+    "url": "/life-game/static/media/medal.9d37f477.svg"
+  }
+]);
